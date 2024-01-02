5ea897d804c4db5fb5159808efbced055619b3fb
xylogan <xy.logandev@gmail.com>
2024-01-02 21:20:10 +0900

    feat : 스프링 프로젝트 기본 세팅 및 의존성 추가 (#3)

---
+++ src/test/java/com/nainga/nainga/NaingaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nainga.nainga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaingaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
