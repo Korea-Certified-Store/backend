37afad7dc1ebb9a6d3adc38141d5d28f2a705d8c
sungjindev <sungjin.developer@gmail.com>
2024-01-03 22:59:39 +0900

    fix: MySQL에 보편적인 기본키 생성 전략으로 수정 (#9)

--- src/main/java/com/nainga/nainga/domain/store/domain/Store.java
+++ src/main/java/com/nainga/nainga/domain/store/domain/Store.java
@@ -1,9 +1,6 @@
 package com.nainga.nainga.domain.store.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -13,7 +10,9 @@ import lombok.*;
 @AllArgsConstructor
 public class Store {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Hibernate 버전 5이상이 되면서 @GeneratedValue의 Defualt strategy인 Auto가 MySQL에 대해 Table 전략을 사용하므로
+    //이를 보편적인 IDENTITY로 명시적으로 변환 시켜주기 위함. Table 전략을 사용하게 되면 Sequence를 모방하기 위해 seq 테이블이 별도로 추가 생성됨.
     @Column(name = "store_id")
     private Long id;
     private String name;    //가게 이름
