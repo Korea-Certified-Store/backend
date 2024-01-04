10a4c5a71dd764170bcd13690901da009bafe49b
sungjindev <sungjin.developer@gmail.com>
2024-01-04 22:30:00 +0900

    feat : 모범 데이터 파싱을 위한 파서와 이를 전달할 데이터 포맷 추가 (#16)

---
+++ src/main/java/com/nainga/nainga/domain/store/application/MobeomDataParser.java
@@ -0,0 +1,15 @@
+package com.nainga.nainga.domain.store.application;
+
+import com.nainga.nainga.domain.store.dto.StoreDataByParser;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+모범 음식점 관련 데이터들을 파싱하기 위한 클래스
+ */
+public class MobeomDataParser {
+    public static List<StoreDataByParser> getAllMobeomStores() {
+
+    }
+}
---
+++ src/main/java/com/nainga/nainga/domain/store/dto/StoreDataByParser.java
@@ -0,0 +1,17 @@
+package com.nainga.nainga.domain.store.dto;
+
+import lombok.Data;
+
+/*
+각 인증제별 데이터셋을 파싱하고 난 뒤 추출한 데이터 형식
+ */
+@Data
+public class StoreDataByParser {
+    String name;    //가게 이름
+    String address;     //가게 주소
+
+    public StoreDataByParser(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
