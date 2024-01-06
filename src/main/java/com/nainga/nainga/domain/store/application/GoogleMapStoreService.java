c0d2ff4ddf991facdf9df282a632e7d3f56767dc
sungjindev <sungjin.developer@gmail.com>
2024-01-06 18:23:03 +0900

    feat : Google Map API 내 places API로부터 place id를 가져오는 메서드 구현 (#16)

--- src/main/java/com/nainga/nainga/domain/store/application/GoogleMapStoreService.java
+++ src/main/java/com/nainga/nainga/domain/store/application/GoogleMapStoreService.java
@@ -1,13 +1,89 @@
 package com.nainga.nainga.domain.store.application;
 
+import com.google.gson.*;
 import com.nainga.nainga.domain.store.dao.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GoogleMapStoreService {
     private final StoreRepository storeRepository;
+
+    @Transactional
+    public void createAllMobeomStores() {
+
+    }
+
+    //이 메서드는 Google Map API 내 "텍스트 검색(신규)"를 활용하여 텍스트 기반으로 검색하고 매칭되는 가게의 places.id를 가져옵니다.
+    //주소나 이름이 잘못됐거나, 폐업을했거나하면 장소가 특정되지 않을 수 있습니다. 이땐, places.id가 반환되지 않습니다.
+    //혹은 흔한 이름이고 해당 주소지 근방에 비슷한 이름들을 가진 가게가 많을 경우 여러 개의 places.id가 반환될 수 있습니다.
+    //따라서, 모호함을 없애기 위해 저희 가게가 정말 잘 찾아진 경우, 즉 places.id가 딱 1개만 반환되었을 때만 DB에 반영합니다.
+    public String getGoogleMapPlacesId(String name, String address) {
+        String reqURL = "https://places.googleapis.com/v1/places:searchText";
+        String textQuery = address + name;
+        String GoogleApiKey = System.getenv("GOOGLE_API_KEY");  //Secrets 보호를 위해 환경 변수 사용
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");  //Google Map API의 정해진 헤더
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("X-Goog-Api-Key", GoogleApiKey);
+            conn.setRequestProperty("X-Goog-FieldMask", "places.id");
+
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("textQuery", textQuery);
+            String jsonData = new Gson().toJson(jsonObject);    //GSON을 활용해서 JSON 포맷의 request body 작성
+
+            OutputStream outputStream = conn.getOutputStream(); //Google Map API로 작성해둔 헤더와 request body를 보냄
+            byte[] bytes = jsonData.getBytes(StandardCharsets.UTF_8);
+            outputStream.write(bytes, 0, bytes.length);
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode != 200) {
+                throw new IllegalStateException("Google Map API 요청 중 오류 발생");
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {    //Response string을 수집
+                result += line;
+            }
+
+            JsonParser parser = new JsonParser();
+            JsonElement jsonElement = parser.parse(result);
+
+            if (jsonElement.isJsonObject()) {   //꼼꼼하게 예외처리해주지 않으면 에러 발생
+                JsonObject resultJsonObject = jsonElement.getAsJsonObject();
+                JsonArray placesJsonArray = resultJsonObject.getAsJsonArray("places");  //Response body에서 원하는 데이터를 파싱
+
+                if (placesJsonArray != null) {  //꼼꼼하게 예외처리해주지 않으면 에러 발생
+                    if (placesJsonArray.size() == 1) {  //검색된 가게가 1개로 정확히 특정되었을 때만 해당 가게의 place id를 리턴
+                        return placesJsonArray.get(0).getAsJsonObject().get("id").getAsString();
+                    }
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;    //검색된 가게가 1개일 때를 제외하고는 null을 리턴
+    }
 }
