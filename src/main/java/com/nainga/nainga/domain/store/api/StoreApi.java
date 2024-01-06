f18b4bb06001257317f328821715a258222484cd
sungjindev <sungjin.developer@gmail.com>
2024-01-06 16:03:12 +0900

    feat : API reponse를 한번 wrapping해줄 Result class 생성 (#16)

--- src/main/java/com/nainga/nainga/domain/store/api/StoreApi.java
+++ src/main/java/com/nainga/nainga/domain/store/api/StoreApi.java
@@ -1,4 +1,11 @@
 package com.nainga.nainga.domain.store.api;
 
+import com.nainga.nainga.domain.store.application.GoogleMapStoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class StoreApi {
+    private final GoogleMapStoreService googleMapStoreService;
 }
