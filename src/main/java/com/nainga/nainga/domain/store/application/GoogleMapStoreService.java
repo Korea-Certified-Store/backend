27e2ba24d40d8a6c38b3b1083dc03fa84fd18597
sungjindev <sungjin.developer@gmail.com>
2024-01-06 19:46:26 +0900

    feat : Google Map API 내 places API로부터 place details를 가져오는 메서드 구현 (#16)

--- src/main/java/com/nainga/nainga/domain/store/application/GoogleMapStoreService.java
+++ src/main/java/com/nainga/nainga/domain/store/application/GoogleMapStoreService.java
@@ -2,6 +2,8 @@ package com.nainga.nainga.domain.store.application;
 
 import com.google.gson.*;
 import com.nainga.nainga.domain.store.dao.StoreRepository;
+import com.nainga.nainga.domain.store.domain.Store;
+import com.nainga.nainga.domain.store.dto.StoreDataByParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,9 +23,22 @@ import java.nio.charset.StandardCharsets;
 public class GoogleMapStoreService {
     private final StoreRepository storeRepository;
 
+    //이 메서드는 Mobeom Excel dataset 파싱을 통해 가게 이름과 주소를 얻고, 이 정보를 바탕으로 Google Map Place Id를 가져옵니다.
+    //그 후 얻어진 Google Map Place Id를 가지고 가게 상세 정보를 Google Map API로부터 가져와 Store DB에 저장합니다.
     @Transactional
     public void createAllMobeomStores() {
+        List<StoreDataByParser> allMobeomStores = MobeomDataParser.getAllMobeomStores("mobeom_test.xlsx");
+        for (StoreDataByParser storeDataByParser : allMobeomStores) {
+            String googleMapPlacesId = getGoogleMapPlacesId(storeDataByParser.getName(), storeDataByParser.getAddress());
 
+            if(googleMapPlacesId == null)   //가져온 Google Map Place Id가 null이라는 것은 가게가 하나로 특정되지 않아 사용할 수 없다는 것을 의미
+                continue;
+
+            JsonObject googleMapPlacesDetail = getGoogleMapPlacesDetail(googleMapPlacesId);
+            if(googleMapPlacesDetail == null)   //Google Map Place Detail을 제대로 불러오지 못했을 경우에는 skip
+                continue;
+
+        }
     }
 
     //이 메서드는 Google Map API 내 "텍스트 검색(신규)"를 활용하여 텍스트 기반으로 검색하고 매칭되는 가게의 places.id를 가져옵니다.
@@ -85,5 +101,47 @@ public class GoogleMapStoreService {
         }
 
         return null;    //검색된 가게가 1개일 때를 제외하고는 null을 리턴
+    }
+
+    public JsonObject getGoogleMapPlacesDetail(String googleMapPlacesId) {
+        String reqURL = "https://places.googleapis.com/v1/places/" + googleMapPlacesId + "?languageCode=ko";
+        String GoogleApiKey = System.getenv("GOOGLE_API_KEY");  //Secrets 보호를 위해 환경 변수 사용
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");  //Google Map API의 정해진 헤더
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("X-Goog-Api-Key", GoogleApiKey);
+            conn.setRequestProperty("X-Goog-FieldMask", "id,displayName,primaryTypeDisplayName,formattedAddress,regularOpeningHours.periods,location,internationalPhoneNumber,photos.name,photos.widthPx,photos.heightPx");
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode != 200) {
+                throw new IllegalStateException("Google Map API 요청 중 오류 발생");
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {    //Response string을 수집
+                result += line;
+            }
+
+            JsonParser parser = new JsonParser();
+            JsonElement jsonElement = parser.parse(result);
+
+            if (jsonElement.isJsonObject()) {
+                return jsonElement.getAsJsonObject();
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;    //Google Map API로 부터 얻은 Response JSON이 올바르지 않을 때 null을 리턴
     }
 }
