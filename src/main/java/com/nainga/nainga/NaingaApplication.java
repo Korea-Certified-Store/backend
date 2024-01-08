2b7d319c2e777197e0da60b4119c440fa3bf78ad
sungjindev <sungjin.developer@gmail.com>
2024-01-08 17:22:38 +0900

    fix : github actions workflow에서도 환경 변수 사용할 수 있도록 수정 (#19)

--- src/main/java/com/nainga/nainga/NaingaApplication.java
+++ src/main/java/com/nainga/nainga/NaingaApplication.java
@@ -2,10 +2,13 @@ package com.nainga.nainga;
 
 import com.nainga.nainga.domain.store.application.GoogleMapStoreService;
 import com.nainga.nainga.domain.store.application.MobeomDataParser;
+import com.nainga.nainga.global.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:env.yml"}, factory = EnvConfig.class)
 public class NaingaApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/nainga/nainga/NaingaApplicationTests.java
+++ src/test/java/com/nainga/nainga/NaingaApplicationTests.java
@@ -2,7 +2,9 @@ package com.nainga.nainga;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {"GOOGLE_API_KEY"})
 @SpringBootTest
 class NaingaApplicationTests {
 
