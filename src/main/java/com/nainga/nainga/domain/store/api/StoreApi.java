1da0d955cd9b8013a2a45505cfd3d7b3694864a9
sungjindev <sungjin.developer@gmail.com>
2024-01-10 21:53:34 +0900

    feat : Swagger에 각 API별 tag, summary, description을 추가 (#28)

--- src/main/java/com/nainga/nainga/domain/store/api/StoreApi.java
+++ src/main/java/com/nainga/nainga/domain/store/api/StoreApi.java
@@ -4,6 +4,8 @@ import com.nainga.nainga.domain.store.application.GoogleMapStoreService;
 import com.nainga.nainga.domain.store.domain.Store;
 import com.nainga.nainga.domain.store.dto.CreateDividedMobeomStoresResponse;
 import com.nainga.nainga.global.util.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +16,17 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class StoreApi {
     private final GoogleMapStoreService googleMapStoreService;
+
+    @Tag(name = "초기 Data 생성")
+    @Operation(summary = "모범 음식점 데이터 생성", description = "[WARNING] DB에 처음으로 모든 모범 음식점 데이터를 주입해주는 API입니다. DB에 엄청난 부하가 가는 작업으로, 합의 없이 실행시켜선 안됩니다!")
     @GetMapping("api/v1/store/mobeom")
     public Result<String> createAllMobeomStores(@RequestParam(value = "fileName") String fileName) {
         googleMapStoreService.createAllMobeomStores(fileName);
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, null);
     }
 
+    @Tag(name = "초기 Data 생성")
+    @Operation(summary = "지정한 Credit까지만 사용하여 모범 음식점 데이터 생성", description = "[WARNING] 지정한 Credit까지만 사용하여 그동안 DB에 모범 음식점 데이터를 주입해주는 API입니다. DB에 엄청난 부하가 가는 작업으로, 합의 없이 실행시켜선 안됩니다!")
     @GetMapping("api/v1/store/dividedMobeom")
     public Result<CreateDividedMobeomStoresResponse> createDividedMobeomStores(@RequestParam(value = "fileName") String fileName, @RequestParam(value = "dollars") double dollars, @RequestParam(value = "startIndex") int startIndex) {
         CreateDividedMobeomStoresResponse response = googleMapStoreService.createDividedMobeomStores(fileName, dollars, startIndex);
--- src/main/java/com/nainga/nainga/domain/storecertification/api/StoreCertificationApi.java
+++ src/main/java/com/nainga/nainga/domain/storecertification/api/StoreCertificationApi.java
@@ -6,6 +6,8 @@ import com.nainga.nainga.domain.storecertification.domain.StoreCertification;
 import com.nainga.nainga.domain.storecertification.dto.StoreCertificationsByLocationRequest;
 import com.nainga.nainga.domain.storecertification.dto.StoreCertificationsByLocationResponse;
 import com.nainga.nainga.global.util.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +22,8 @@ public class StoreCertificationApi {
     private final StoreCertificationService storeCertificationService;
 
     //북서쪽, 남동쪽 좌표를 받아 두 좌표로 만들어지는 가장 작은 사각형 내 모든 가게 상세 정보를 반환
+    @Tag(name = "가게 상세 정보")
+    @Operation(summary = "사용자 위치 기반 가게 상세 정보 제공", description = "사용자 위치를 기준으로 좌상단, 우하단 위도 경도 좌표를 넘겨 받아 두 좌표로 만들 수 있는 최소 크기의 사각형 범위 내 모든 가게의 상세 정보를 전달해줍니다.")
     @PostMapping("api/v1/storecertification/byLocation")
     public Result<List<StoreCertificationsByLocationResponse>> findStoreCertificationsByLocation(@RequestBody StoreCertificationsByLocationRequest storeCertificationsByLocationRequest) {
         List<StoreCertification> storeCertificationsByLocation = storeCertificationService.findStoreCertificationsByLocation(storeCertificationsByLocationRequest.getNorthWestLocation(), storeCertificationsByLocationRequest.getSouthEastLocation());
