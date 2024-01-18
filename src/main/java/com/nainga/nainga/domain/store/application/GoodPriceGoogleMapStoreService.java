4d4ef045bbc2641a2f742c46d235e2a93a5fa004
sungjindev <sungjin.developer@gmail.com>
2024-01-18 18:34:16 +0900

    feat : MobeomGoogleMapStoreService에 GCS 이미지 저장 로직 구현 (#54)

--- src/main/java/com/nainga/nainga/domain/store/application/GoodPriceGoogleMapStoreService.java
+++ src/main/java/com/nainga/nainga/domain/store/application/GoodPriceGoogleMapStoreService.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
