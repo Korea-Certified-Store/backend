caf953f8841668056d8e16ff849065ff294b861e
sungjindev <sungjin.developer@gmail.com>
2024-01-25 18:59:51 +0900

    feat : 공통 에러에 대한 코드와 메시지를 처리할 CommonErrorCode 구현 (#74)

---
+++ src/main/java/com/nainga/nainga/global/exception/CommonErrorCode.java
@@ -0,0 +1,16 @@
+package com.nainga.nainga.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonErrorCode implements ErrorCode {
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 파라미터입니다."),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 리소스가 존재하지 않습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부적인 서버 오류입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
