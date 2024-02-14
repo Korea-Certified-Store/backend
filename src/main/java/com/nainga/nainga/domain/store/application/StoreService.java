20bdac9a06269ebbf29aae8707b2f1cc53d758c8
sungjindev <sungjin.developer@gmail.com>
2024-02-14 21:47:53 +0900

    feat : 검색어 자동 완성을 위해 모든 가게명에 대해 음절 단위로 잘라 Redis에 저장하는 로직 구현 (#95)

--- src/main/java/com/nainga/nainga/domain/store/application/StoreService.java
+++ src/main/java/com/nainga/nainga/domain/store/application/StoreService.java
@@ -1,6 +1,7 @@
 package com.nainga.nainga.domain.store.application;
 
 import com.nainga.nainga.domain.store.dao.StoreRepository;
+import com.nainga.nainga.domain.store.domain.Store;
 import com.nainga.nainga.domain.storecertification.domain.StoreCertification;
 import com.nainga.nainga.global.application.RedisSortedSetService;
 import jakarta.annotation.PostConstruct;
@@ -21,12 +22,16 @@ public class StoreService {
 
     @PostConstruct
     public void init() {    //이 Service Bean이 생성된 이후에 검색어 자동 완성 기능을 위한 데이터들을 Redis에 저장 (Redis는 인메모리 DB라 휘발성을 띄기 때문)
-        int pageSize = 1000;
-        int page = 0;
+        saveAllSubstring(storeRepository.findAllDisplayName()); //MySQL DB에 저장된 모든 가게명을 음절 단위로 잘라 모든 Substring을 Redis에 저장해주는 로직
+    }
 
-        while (true) {
-            storeRepository
+    private void saveAllSubstring(List<Store> allDisplayName) { //MySQL DB에 저장된 모든 가게명을 음절 단위로 잘라 모든 Substring을 Redis에 저장해주는 로직
+        for (Store displayName : allDisplayName) {
+            redisSortedSetService.addToSortedSet(displayName.getDisplayName() + "*");   //완벽한 형태의 단어일 경우에는 *을 붙여 구분
+
+            for (int i = displayName.getDisplayName().length()-1; i > 0; --i) { //음절 단위로 잘라서 모든 Substring 구하기
+                redisSortedSetService.addToSortedSet(displayName.getDisplayName().substring(0, i)); //곧바로 redis에 저장
+            }
         }
-
     }
 }
