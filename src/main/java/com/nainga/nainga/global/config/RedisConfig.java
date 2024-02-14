fa5fefeceeeb5b98051cfb33f76cf01011a32aff
sungjindev <sungjin.developer@gmail.com>
2024-02-14 16:49:49 +0900

    feat : Redis 연결을 위한 기본 Config 클래스 추가 (#95)

---
+++ src/main/java/com/nainga/nainga/global/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.nainga.nainga.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {  //Redis 연결을 위한 기본 설정
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                new RedisStandaloneConfiguration(host, port)
+        );
+    }
+}
