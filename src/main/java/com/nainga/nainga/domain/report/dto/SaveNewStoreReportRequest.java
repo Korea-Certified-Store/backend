49ccb30ff5e8ed7cd51cd72b95850c259af4c129
sungjindev <sungjin.developer@gmail.com>
2024-02-17 23:31:52 +0900

    feat : SaveNewStoreReportRequest 구현 (#105)

--- src/main/java/com/nainga/nainga/domain/report/dto/SaveNewStoreReportRequest.java
+++ src/main/java/com/nainga/nainga/domain/report/dto/SaveNewStoreReportRequest.java
@@ -1,12 +1,16 @@
 package com.nainga.nainga.domain.report.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SaveNewStoreReportRequest {
+    @NotNull
     private String storeName;   //가게 이름
+    @NotNull
     private String formattedAddress;    //가게 주소
+    @NotNull
     private List<String> certifications;    //가게가 가지고 있는 인증제 이름 리스트
 }
