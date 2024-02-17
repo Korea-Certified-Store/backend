7ed574df7957a26487ecb01e156282685835b54b
sungjindev <sungjin.developer@gmail.com>
2024-02-18 01:10:57 +0900

    feat : 편의를 위해 Springdoc Request Body에 Default value 설정 (#105)

--- src/main/java/com/nainga/nainga/domain/report/dto/SaveNewStoreReportRequest.java
+++ src/main/java/com/nainga/nainga/domain/report/dto/SaveNewStoreReportRequest.java
@@ -1,5 +1,6 @@
 package com.nainga.nainga.domain.report.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -8,9 +9,12 @@ import java.util.List;
 @Data
 public class SaveNewStoreReportRequest {
     @NotNull
+    @Schema(defaultValue = "가게 이름", description = "새로 등록하고자 하는 가게 이름")
     private String storeName;   //가게 이름
     @NotNull
+    @Schema(defaultValue = "주소", description = "새로 등록하고자 하는 가게 주소")
     private String formattedAddress;    //가게 주소
     @NotNull
+    @Schema(defaultValue = "[\"착한가격업소\", \"모범음식점\", \"안심식당\"]", description = "새로 등록할 가게가 가지고 있는 인증제들의 이름을 담은 리스트. 착한가격업소, 모범음식점, 안심식당이 아닌 경우 예외 발생")
     private List<String> certifications;    //가게가 가지고 있는 인증제 이름 리스트
 }
