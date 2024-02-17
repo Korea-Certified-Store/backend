48bdabc161d50b9b27a1a3f498ef13c29ae7ea6c
sungjindev <sungjin.developer@gmail.com>
2024-02-18 03:55:52 +0900

    feat : reportId로 report 내용을 조회하는 로직에 대한 테스트 코드 작성 (#105)

--- src/test/java/com/nainga/nainga/domain/report/application/ReportServiceTest.java
+++ src/test/java/com/nainga/nainga/domain/report/application/ReportServiceTest.java
@@ -36,9 +36,8 @@ class ReportServiceTest {
         SaveNewStoreReportRequest saveNewStoreReportRequest2 = new SaveNewStoreReportRequest("가게2", "주소2", List.of("착한가격업"));   //인증제 이름이 잘못되었을 때
 
         //when
-        reportService.saveNewStoreReport(saveNewStoreReportRequest1);
-        List<Report> reports = reportRepository.findAll();
-        NewStoreReport report = (NewStoreReport) reports.stream().findAny().get();
+        Long report1Id = reportService.saveNewStoreReport(saveNewStoreReportRequest1);
+        NewStoreReport report = (NewStoreReport) reportService.findById(report1Id);
 
         //then
         assertArrayEquals(report.getCertifications().toArray(), saveNewStoreReportRequest1.getCertifications().toArray());
@@ -56,17 +55,36 @@ class ReportServiceTest {
         SaveSpecificStoreReportRequest saveSpecificStoreReportRequest3 = new SaveSpecificStoreReportRequest("xxx", 12345L, "내용3");  //잘못된 dtype
 
         //when
-        reportService.saveSpecificStoreReport(saveSpecificStoreReportRequest1);
-        reportService.saveSpecificStoreReport(saveSpecificStoreReportRequest2);
-        List<Report> reports = reportRepository.findAll();
+        Long report1Id = reportService.saveSpecificStoreReport(saveSpecificStoreReportRequest1);
+        Long report2Id = reportService.saveSpecificStoreReport(saveSpecificStoreReportRequest2);
+        DelSpecificStoreReport report1 = (DelSpecificStoreReport) reportService.findById(report1Id);
+        FixSpecificStoreReport report2 = (FixSpecificStoreReport) reportService.findById(report2Id);
 
         //then
-        DelSpecificStoreReport report1 = (DelSpecificStoreReport) reports.get(0);
-        FixSpecificStoreReport report2 = (FixSpecificStoreReport) reports.get(1);
         assertThat(report1.getStoreId()).isEqualTo(saveSpecificStoreReportRequest1.getStoreId());
         assertThat(report2.getContents()).isEqualTo(saveSpecificStoreReportRequest2.getContents());
         assertThatThrownBy(() -> reportService.saveSpecificStoreReport(saveSpecificStoreReportRequest3))    //잘못된 dtype이라서 예외가 터져야함
                 .isInstanceOf(GlobalException.class);
+    }
+
+    @Test
+    public void findById() throws Exception {
+        //given
+        SaveNewStoreReportRequest saveNewStoreReportRequest1 = new SaveNewStoreReportRequest("가게1", "주소1", List.of("착한가격업소", "모범음식점")); //정상적인 테스트 케이스
+        SaveSpecificStoreReportRequest saveSpecificStoreReportRequest1 = new SaveSpecificStoreReportRequest("del", 123L, "내용1");    //정상적인 테스트 케이스
+
+        //when
+        Long report1Id = reportService.saveNewStoreReport(saveNewStoreReportRequest1);
+        Long report2Id = reportService.saveSpecificStoreReport(saveSpecificStoreReportRequest1);
+        NewStoreReport report1 = (NewStoreReport) reportService.findById(report1Id);
+        DelSpecificStoreReport report2 = (DelSpecificStoreReport) reportService.findById(report2Id);
+
+        //then
+        assertThat(report1.getFormattedAddress()).isEqualTo(saveNewStoreReportRequest1.getFormattedAddress());
+        assertArrayEquals(report1.getCertifications().toArray(), saveNewStoreReportRequest1.getCertifications().toArray());
+        assertThat(report1.getStoreName()).isEqualTo(saveNewStoreReportRequest1.getStoreName());
+        assertThat(report2.getContents()).isEqualTo(saveSpecificStoreReportRequest1.getContents());
+        assertThat(report2.getStoreId()).isEqualTo(saveSpecificStoreReportRequest1.getStoreId());
     }
 
 }
